038b9c3c5a47a7a4c2027af1948ad8cb0059db9f
Andreas Joelsson <andreas.joelsson@gmail.com>
2016-05-04 06:27:40 +0200

    Fixed dependency for java tools

--- pre/src/main/java/io/scrier/tablets/pre/Precondition.java
+++ pre/src/main/java/io/scrier/tablets/pre/Precondition.java
@@ -10,11 +10,34 @@ import java.util.Map;
  */
 public class Precondition implements Taglet {
 
+    private static final String X = "X"; // (disable tag)
+    private static final String a = "a"; // (all)
+    private static final String o = "o"; // (overview)
+    private static final String p = "p"; // (packages)
+    private static final String t = "t"; // (types, that is classes and interfaces)
+    private static final String c = "c"; // (constructors)
+    private static final String m = "m"; // (methods)
+    private static final String f = "f"; // (fields)
+
     private static final String NAME = "scrier.pre";
     private static final String HEADER = "Preconditions:";
+    private static final String PLACEMENT = "a";
+
+    private static final String NAME_OVERRIDE = "scrier.name";
+    private static final String HEADER_OVERRIDE = "scrier.header";
+    private static final String PLACEMENT_OVERRIDE = "scrier.placement";
+
+    private final String name;
+    private final String header;
+    private final String placement;
 
     public Precondition() {
-
+        this.name = ( null == System.getProperty(NAME_OVERRIDE)
+                ? NAME : System.getProperty(NAME_OVERRIDE) );
+        this.header = ( null == System.getProperty(HEADER_OVERRIDE)
+                ? HEADER : System.getProperty(HEADER_OVERRIDE) );
+        this.placement = ( null == System.getProperty(PLACEMENT_OVERRIDE)
+                ? PLACEMENT : System.getProperty(PLACEMENT_OVERRIDE) );
     }
 
     public static void register(Map tagletMap) {
@@ -26,27 +49,39 @@ public class Precondition implements Taglet {
     }
 
     public boolean inField() {
-        return false;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(f) ||
+                        this.placement.equals(a) );
     }
 
     public boolean inConstructor() {
-        return false;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(c) ||
+                        this.placement.equals(a) );
     }
 
     public boolean inMethod() {
-        return true;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(m) ||
+                        this.placement.equals(a) );
     }
 
     public boolean inOverview() {
-        return false;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(o) ||
+                        this.placement.equals(a) );
     }
 
     public boolean inPackage() {
-        return false;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(p) ||
+                        this.placement.equals(a) );
     }
 
     public boolean inType() {
-        return false;
+        return !this.placement.equals(X) &&
+                ( this.placement.equals(t) ||
+                        this.placement.equals(a) );
     }
 
     public boolean isInlineTag() {
@@ -54,16 +89,16 @@ public class Precondition implements Taglet {
     }
 
     public String getName() {
-        return NAME;
+        return this.name;
     }
 
     public String toString(Tag tag) {
-        return "<dt><span class=\"paramLabel\">" + HEADER + "</span></dt>" +
+        return "<dt><span class=\"paramLabel\">" + this.header + "</span></dt>" +
                 "<dd><code>" + tag.text() + "</code></dd>";
     }
 
     public String toString(Tag[] tags) {
-        StringBuilder builder = new StringBuilder("<dt><span class=\"paramLabel\">" + HEADER + "</span></dt>");
+        StringBuilder builder = new StringBuilder("<dt><span class=\"paramLabel\">" + this.header + "</span></dt>");
         for( Tag tag : tags ) {
             builder.append("<dd><code>" + tag.text() + "</code></dd>");
         }
